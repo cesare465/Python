34cb71c0722eb177a575f879629b3c44d8117854
cesare465 <qmirzaxadjayev@gmail.com>
2026-02-18 15:54:27 +0500

    Add files via upload

---
+++ bablesort.py
@@ -0,0 +1,12 @@
+def buble_sort(a):
+    n=len(a)
+    for i in range(n-1,0,-1):
+        for i in range(i):
+            for j in range:
+                if a[j]>a[j+1]:
+                    a[j,a[j+1],a[j]]
+                    
+
+a=[55,11,44,22,88]
+
+
